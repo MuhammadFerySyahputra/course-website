27c5f588fbc798a6165b68ffcb55fb128a218e9e
MuhammadFerySyahputra <143172394+MuhammadFerySyahputra@users.noreply.github.com>
2024-01-07 12:20:55 +0700

    Add files via upload

---
+++ src/js/script.js
@@ -0,0 +1,45 @@
+var swiper = new Swiper(".slide-content2", {
+  slidesPerView: 3,
+  centeredSlides: true,
+  slidesPerGroupSkip: 1,
+  spaceBetween: 30,
+  grabCursor: true,
+  dynamicBullets: true,
+  loop: true,
+  pagination: {
+    el: ".swiper-pagination",
+    clickable: true,
+  },
+  navigation: {
+    nextEl: ".swiper-button-next",
+    prevEl: ".swiper-button-prev",
+  },
+  keyboard: {
+    enabled: true,
+  },
+  autoplay: {
+    delay: 5000,
+    disableOnInteraction: false,
+  },
+});
+
+var swiper = new Swiper(".content-class", {
+  slidesPerView: 3,
+  centeredSlides: true,
+  slidesPerGroupSkip: 1,
+  spaceBetween: 30,
+  grabCursor: true,
+  dynamicBullets: true,
+  loop: true,
+  pagination: {
+    el: ".swiper-pagination",
+    clickable: true,
+  },
+  navigation: {
+    nextEl: ".swiper-button-next",
+    prevEl: ".swiper-button-prev",
+  },
+  keyboard: {
+    enabled: true,
+  },
+});
